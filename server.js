63523db2f89b4aee926d2bb5b5a60e9f7576f13e
marc <marcdwest@gmail.com>
2020-03-09 16:22:10 -0500

    add routes

--- server.js
+++ server.js
@@ -4,20 +4,25 @@ const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handle = app.getRequestHandler();
+const routes = require('./routes');
+const handle = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
   createServer((req, res) => {
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
+
     const parsedUrl = parse(req.url, true);
     const { pathname, query } = parsedUrl;
 
     if (pathname === '/a') {
+      console.log('serving other requests 1');
       app.render(req, res, '/b', query);
     } else if (pathname === '/b') {
+      console.log('serving other requests 2');
       app.render(req, res, '/a', query);
     } else {
+      console.log('serving other requests 3');
       handle(req, res, parsedUrl);
     }
   }).listen(3000, err => {
