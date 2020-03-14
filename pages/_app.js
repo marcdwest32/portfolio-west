590e7fc65a17f6097558bbeb6911796467ad1692
marc <marcdwest@gmail.com>
2020-03-13 23:04:00 -0500

    wrap in auth context

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
+import React from 'react';
 import App from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.scss';
+import { Auth0Provider } from '../react-auth0-spa';
+import config from '../auth_config.json';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      redirect_uri={'http://localhost:3000/callback'}
+      //   onRedirectCallback={onRedirectCallback}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
 
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
 MyApp.getInitialProps = async appContext => {
   // calls page's `getInitialProps` and fills `appProps.pageProps`
   const appProps = await App.getInitialProps(appContext);
